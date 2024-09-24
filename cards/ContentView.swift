a9c8dc4699f4b38a10a507ca291c886c9ea25e26
cc23337 <tiago.cps123@gmail.com>
2024-09-24 16:09:38 -0300

    Initial Commit

---
+++ cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cards
+//
+//  Created by TIAGO COSTA CONCEICAO on 24/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by TIAGO COSTA CONCEICAO on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
